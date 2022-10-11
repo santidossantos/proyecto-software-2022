59cdd0edaeef5726898451eca28c604868035ef1
Santiago dos Santos <santiiagodossantos@icloud.com>
2022-10-11 15:42:10 -0300

    Agregue logo al PDF y nombre de archivo al descargar

--- admin/src/web/utils/exporters/pdf.py
+++ admin/src/web/utils/exporters/pdf.py
@@ -5,4 +5,8 @@ from flask import render_template
 def generate_pdf_file(records):
 
     html = render_template("associates/report.html", associates=records)
-    return render_pdf(HTML(string=html))
+    return render_pdf(
+        HTML(string=html),
+        automatic_download=False,
+        download_filename="listado_socios.pdf",
+    )

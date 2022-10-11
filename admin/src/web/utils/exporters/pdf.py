80cff7254cb84e6dfc06dc47fbba15ebad1deb3f
Santiago dos Santos <santiiagodossantos@icloud.com>
2022-10-11 13:54:45 -0300

    Estilos reporte en PDF

--- admin/src/web/utils/exporters/pdf.py
+++ admin/src/web/utils/exporters/pdf.py
@@ -1,11 +1,8 @@
 from flask_weasyprint import HTML, render_pdf
 from flask import render_template
-from src.core import auth # vuela
 
 
 def generate_pdf_file(records):
-    
-    users = paginated_users = auth.list_users(page_num=1, per_page=4)  # solo para probar
 
-    html = render_template("users/users_list.html", users=users)
+    html = render_template("associates/report.html", associates=records)
     return render_pdf(HTML(string=html))

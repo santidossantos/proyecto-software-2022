9fe2ae4e2161ee8b99edad1fccf484588d13f228
Santiago dos Santos <santiiagodossantos@icloud.com>
2022-10-15 23:49:09 -0300

    deshabilito archivo pdf

--- admin/src/web/utils/exporters/pdf.py
+++ admin/src/web/utils/exporters/pdf.py
@@ -1,12 +1,13 @@
-from flask_weasyprint import HTML, render_pdf
-from flask import render_template
+#from flask_weasyprint import HTML, render_pdf
+#from flask import render_template
 
 
 def generate_pdf_file(records):
 
-    html = render_template("associates/report.html", associates=records)
-    return render_pdf(
-        HTML(string=html),
-        automatic_download=False,
-        download_filename="listado_socios.pdf",
-    )
+#    html = render_template("associates/report.html", associates=records)
+#   return render_pdf(
+#        HTML(string=html),
+#       automatic_download=False,
+#       download_filename="listado_socios.pdf",
+#   )
+    return "Exportando a PDF..."

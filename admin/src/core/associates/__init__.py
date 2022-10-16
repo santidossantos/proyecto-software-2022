f49653671d8b2aa38e8edb417c138304e7a8ddb2
Santiago dos Santos <santiiagodossantos@icloud.com>
2022-10-15 23:00:22 -0300

    Revert "Merge branch 'development' into 'main'"

--- admin/src/core/associates/__init__.py
+++ admin/src/core/associates/__init__.py
@@ -6,12 +6,6 @@ def list_associate(page_num, per_page):
     return Associate.query.paginate(page_num, per_page, True)
 
 
-def list_associate_filtered(search_filter, active_filter):
-    return Associate.query.filter(Associate.active == active_filter).filter(
-        Associate.last_name.ilike(f'%{search_filter}%')
-    )
-
-
 def create_user(**kwargs):
     associate = Associate(**kwargs)
     db.session.add(associate)
@@ -34,25 +28,8 @@ def update_associate(**kwargs):
 def usWithUserEmail(email):
     return Associate.query.filter_by(email=email).first()
 
-
 def delete_user(id):
     user = Associate.query.get(id)
     db.session.delete(user)
     db.session.commit()
     return user
-
-
-def usWithUserEmail(email):
-    return Associate.query.filter_by(email=email).first()
-
-
-def searchByStatus(search_filter, page_num, per_page):
-    return Associate.query.filter(Associate.active == search_filter).paginate(
-        page_num, per_page, True
-    )
-
-
-def searchBySurname(surname, page_num, per_page):
-    return Associate.query.filter(Associate.last_name == surname).paginate(
-        page_num, per_page, True
-    )

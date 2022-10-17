484ae830b658d6fa69d587ca561cc348adfa6e88
Martin Ezequiel Rodriguez <martinrodrig2001@gmail.com>
2022-10-16 23:59:56 -0300

    guardo para solucionar error

--- admin/src/core/disciplines/discipline.py
+++ admin/src/core/disciplines/discipline.py
@@ -1,4 +1,5 @@
 from src.core.database import db
+from src.core.auth.user import User
 
 class Discipline(db.Model):
 
@@ -10,3 +11,9 @@ class Discipline(db.Model):
     daysAndHours = db.Column(db.String(100))
     monthlyCost = db.Column(db.String(20))
     active = db.Column(db.Boolean(), default=False)
+
+UserDiscipline = db.Table(
+    "user_discipline",
+    db.Column("user_id", db.Integer, db.ForeignKey("users.id")),
+    db.Column("discipline_id", db.Integer, db.ForeignKey("discipline.id")),
+)
---
+++ admin/src/web/controllers/inscription.py
@@ -0,0 +1,25 @@
+from flask import Blueprint
+from flask import render_template
+from flask import request
+from flask import flash
+from flask import url_for
+from flask import redirect
+
+from src.core import associates
+from src.core import inscription
+
+inscription_blueprint = Blueprint("inscription", __name__, url_prefix="/inscription")
+
+@inscription_blueprint.get("/")
+@inscription_blueprint.get("/<int:page_num>")
+def inscription(page_num=1, per_page=4):
+    paginated_associates = associates.list_associate(
+        page_num=page_num, per_page=per_page
+    )
+    return render_template("disciplines/inscriptions.html", associates=paginated_associates
+    )
+
+inscription_blueprint.route("/inscription/<id>")
+def inscription(id):
+    associate = associates.get_associate(id=id)
+

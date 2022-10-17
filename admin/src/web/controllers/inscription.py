8c3bfc9648b203787f43b2dbdee0f5b19fc5dc4b
Martin Ezequiel Rodriguez <martinrodrig2001@gmail.com>
2022-10-17 15:55:51 -0300

    guardo cambios inscripcion funcionando, le falta

--- admin/src/web/controllers/inscription.py
+++ admin/src/web/controllers/inscription.py
@@ -7,22 +7,23 @@ from flask import url_for
 from flask import redirect
 
 from src.core import associates
-from src.core import inscription
 from src.core import disciplines
 
 inscription_blueprint = Blueprint("inscription", __name__, url_prefix="/inscription")
 
 @inscription_blueprint.get("/")
 @inscription_blueprint.get("/<int:page_num>")
-def inscription(page_num=1, per_page=4):
+@inscription_blueprint.get("/<int:id>")
+def inscription(id,page_num=1, per_page=4):
     paginated_associates = associates.list_associate(
         page_num=page_num, per_page=per_page
     )
-    return render_template("disciplines/inscriptions.html", associates=paginated_associates)
+    return render_template("disciplines/inscriptions.html", associates=paginated_associates, idDisciplina=id)
 
-@inscription_blueprint.route("/doInscription/<id>")
-def doInscription(id):
-    disciplines.createInscription(idAsociado=id, idDisciplina=1)
+@inscription_blueprint.route("/doInscription/<id>/<idDisciplina>")
+def doInscription(id, idDisciplina):
+    print(id)
+    disciplines.createInscription(idAsociado=id, idDisciplina=idDisciplina)
     return redirect((url_for("disciplines.discipline_index")))
 
     

2d1660f6329846c9808b0a9f4d0f08c9f4665684
Lorena Othaz <lorth87@hotmail.com>
2022-10-19 20:31:29 -0300

    v2

--- admin/src/core/associates/__init__.py
+++ admin/src/core/associates/__init__.py
@@ -7,12 +7,14 @@ def list_associate(page_num, per_page):
 
 
 def list_associateActive(page_num, per_page):
-    return Associate.query.filter(Associate.active == True).paginate(page_num, per_page, True)
+    return Associate.query.filter(Associate.active == True).paginate(
+        page_num, per_page, True
+    )
 
 
 def list_associate_filtered(search_filter, active_filter):
     return Associate.query.filter(Associate.active == active_filter).filter(
-        Associate.last_name.ilike(f'%{search_filter}%')
+        Associate.last_name.ilike(f"%{search_filter}%")
     )
 
 
@@ -61,9 +63,11 @@ def searchBySurname(surname, page_num, per_page):
         page_num, per_page, True
     )
 
+
 def is_defaulter(id):
     associate = Associate.query.get(id)
     return associate.defaulter
+
 
 def is_active(id):
     associate = Associate.query.get(id)
--- admin/src/core/payment/payment.py
+++ admin/src/core/payment/payment.py
@@ -1,18 +1,40 @@
 from src.core.database import db
 from src.core.associates import associate
 import datetime
+import enum
+
+
+class Mes(enum.Enum):
+    E = "Enero"
+    F = "Febrero"
+    M = "Marzo"
+
+
+class State(enum.Enum):
+    P = "Pagado"
+    I = "Impago"
 
 
 class Payment(db.Model):
 
     __tablename__ = "payments"
     id = db.Column(db.Integer, primary_key=True, unique=True)
+    associated_id = db.Column(db.Integer, db.ForeignKey("associates.id"))
+    mes = db.Column(db.Enum(Mes), nullable=False)
     total = db.Column(db.Integer)
-    date = db.Column(
+    state = db.Column(db.Enum(State), default="I", nullable=False)
+
+    date_pago = db.Column(
         db.DateTime, default=datetime.datetime.now(), onupdate=datetime.datetime.now()
     )
     create_at = db.Column(
         db.DateTime, default=datetime.datetime.now(), onupdate=datetime.datetime.now()
     )
 
-    associated_id = db.Column(db.Integer, db.ForeignKey("associates.id"))
+
+def create_month_registers(id_user):
+    mes = ["E", "F", "M"]
+    for i in mes:
+        payment = Payment(associated_id=id_user, mes=i, total=0)
+        db.session.add(payment)
+        db.session.commit()

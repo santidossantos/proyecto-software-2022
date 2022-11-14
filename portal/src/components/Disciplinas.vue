8f712e278edd1c0f37a0d9a1658e6d7f5dbbd705
Sofia Marcela Raciti <sofiracitiii@gmail.com>
2022-11-13 21:16:31 -0300

    agrego componentes en el front y cors al backend

---
+++ portal/src/components/Disciplinas.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="hello">
+      <h1>{{disciplinas}}</h1>
+    </div>
+  </template>
+
+<script>
+import axios from 'axios';
+  export default {
+  data() {
+    return {
+      disciplinas: [],
+      errors: []
+   }
+  },
+  // Fetches posts when the component is created.
+  created() {
+    axios.get('http://127.0.0.1:5000/api/club/disciplines')
+    .then(response => {
+    // JSON responses are automatically parsed.
+    this.disciplinas = response.data;
+    })
+    .catch(e => {
+    this.errors.push(e)
+    })
+  }
+}
+</script>
--- portal/src/views/AboutView.vue
+++ portal/src/views/AboutView.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Listado de Disciplinas</h1>
+    <Disciplinas />
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import Disciplinas from "@/components/Disciplinas.vue";
+
+export default {
+  name: "AboutView",
+  components: {
+    Disciplinas
+  },
+};
+</script>

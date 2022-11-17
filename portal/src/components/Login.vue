bb170ea0e82d74eefb192dc4e794784ce8329c60
Sofia Marcela Raciti <sofiracitiii@gmail.com>
2022-11-17 12:22:56 -0300

    arreglo del token

--- portal/src/components/Login.vue
+++ portal/src/components/Login.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="hello">
         <h1>{{ token }}</h1>
-
         <form action="">
             <label for="nombre">
                 <span>¿Cuál es tu nombre?</span>
@@ -12,48 +11,20 @@
                 <input type="text" id="password" placeholder="password">
             </label>
         </form>
-
-
     </div>
 </template>
   
 <script>
 import axios from 'axios';
 
-const apiService = axios.create({
-    baseURL: 'http://localhost:5000/api',
-    withCredentials: true,
-    xsrfCookieName: 'csrf_access_token'
-});
-
-
-
-const actions = {
-    async loginUser(user) {
-        await apiService.post('/auth/token', user)
-        await fetchUser()
-    },
-    async fetchUser() {
-        await apiService.get('/auth/user_jwt')
-            .then(({ data }) => console.log(data))
-    },
-
-    //  async logoutUser({ commit }) {
-    //  await apiService.get('/auth/logout_jwt');
-    //  commit('logoutUserState');
-    //  }
-};
-
-
 export default {
     data() {
         return {
-            token: {},
+            token: "",
             errores: []
         }
     },
     // Fetches posts when the component is created.
-
     created() {
         const user = {
             "email": "admin@gmail.com",
@@ -61,10 +32,17 @@ export default {
         }
         actions.loginUser(user)
     }
-
-
 }
 
+const apiService = axios.create({
+    baseURL: 'http://localhost:5000/api',
+    xsrfCookieName: 'csrf_access_token'
+});
 
-
+const actions = {
+    async loginUser(user) {
+        const response = await apiService.post('/auth/token', user)
+        console.log(response.data)
+    }
+};
 </script>

489e7b67b0df8743d3512eb9192fe2c4f11fe41d
Santiago dos Santos <santiiagodossantos@icloud.com>
2022-11-20 21:21:55 -0300

    prueba cors

--- portal/src/services/api.js
+++ portal/src/services/api.js
@@ -3,7 +3,9 @@ import axios from "axios";
 export const apiService = axios.create({
   baseURL: process.env.VUE_APP_RUTA,
   xsrfCookieName: "csrf_access_token",
+  
   headers: {
+    "Access-Control-Allow-Origin": "*,*",
     Authorization: `Bearer ${localStorage.getItem("token")}`,
   },
 });

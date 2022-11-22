a3728d1129a77a73ed2324963bf783009a5d17c7
Sofia Marcela Raciti <sofiracitiii@gmail.com>
2022-11-21 21:11:06 -0300

    saco libreria que no se usa

--- portal/src/components/Payment.vue
+++ portal/src/components/Payment.vue
@@ -9,18 +9,27 @@
     </thead>
     <tbody>
       <tr v-for="(pay, index) in payment" :key="index">
-        <td>{{ pay.mes }}</td>
+        <td>Noviembre</td>
         <td>{{ total / this.payment.length }}</td>
-        <tr v-if="(pay.mes == this.payment[index].mes)">
-          <button v-if="pay.total == 0" @click="registerPayment(pay.mes, total)">Pagar</button>
-      </tr>
-      <button v-else disabled>Pagar</button>
-      <button v-if="pay.total != 0" @click="emitirComporbante(pay.mes, total / this.payment.length)">Emitir
-        comprobante</button>
+        <td>
+          <button class="btn btn-success"
+            v-if="pay.total == 0"
+            @click="registerPayment(pay.mes, total)"
+          >
+            Pagar
+          </button>
+          <button
+            class="btn btn-info"
+            v-if="pay.total != 0"
+            @click="emitirComporbante(pay.mes, total / this.payment.length)"
+          >
+            Emitir comprobante
+          </button>
+        </td>
       </tr>
       <div ref="content" class="hidden">
         <h1>Hola</h1>
-        <br>
+        <br />
         <h1>Como va</h1>
         <span>prueba</span>
       </div>
@@ -39,7 +48,6 @@ export default {
       payment: [],
       total: 0,
       errores: [],
-
     };
   },
   async created() {
@@ -47,7 +55,6 @@ export default {
       .get("me/payments")
       .then((response) => {
         this.payment = response.data;
-
       })
       .catch((e) => {
         this.errores.push(e);
@@ -65,22 +72,20 @@ export default {
   methods: {
     registerPayment(mes, total) {
       if (this.payment.length != 0) {
-        total = total / this.payment.length
-
+        total = total / this.payment.length;
       }
       const formData = {
         month: mes,
         total: total,
       };
-      console.log(formData)
+      console.log(formData);
       apiService
         .post("me/payments", formData)
         .then((response) => {
-          // JSON responses are automatically parsed.
           console.log(response);
           if (response.status === 200) {
-            alert("Se generò el pago")
-            this.$router.push("/perfil")
+            alert("Se generò el pago");
+            this.$router.push("/perfil");
           }
         })
         .catch((e) => {
@@ -89,25 +94,18 @@ export default {
     },
     emitirComporbante(mes, total) {
       const doc = new jsPDF();
-      const mess = mes;
-      /** WITHOUT CSS */
       const contentHtml = this.$refs.content.textContent + mes + total;
-      //const contentHtml2 = contentHtml.textHTML = mes
-      //const contentHtml3 = contentHtml2.textHTML = total
       doc.text(contentHtml, 15, 15, {
-        width: 170
+        width: 170,
       });
       doc.save("Comprobante.pdf");
     },
-
   },
-
-}
-
+};
 </script>
 
 <style>
 .hidden {
-  display: none
+  display: none;
 }
 </style>

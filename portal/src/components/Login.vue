bea199627a20ad906eacc78dd51879d5537f9233
Sofia Marcela Raciti <sofiracitiii@gmail.com>
2022-11-23 22:39:43 -0300

    saco comentario login viejo

--- portal/src/components/Login.vue
+++ portal/src/components/Login.vue
@@ -29,28 +29,6 @@
       <input class="btn mt-3" type="submit" value="Login">
     </form>
   </div>
-  <!-- <div class="hello">
-    <form action class="form" @submit.prevent="register">
-      <label class="form-label" for="#email">Nombre de Usuario:</label>
-      <input
-        v-model="username"
-        class="form-input"
-        type="text"
-        id="username"
-        required
-        placeholder="Email"
-      />
-      <label class="form-label" for="#password">Password:</label>
-      <input
-        v-model="password"
-        class="form-input"
-        type="password"
-        id="password"
-        placeholder="Password"
-      />
-      <input class="form-submit" type="submit" value="Login" />
-    </form>
-  </div> -->
 </template>
 
 <script>

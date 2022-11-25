a217c68c1b36cce34de85ebdba5498434ae5040a
Martin Ezequiel Rodriguez <martinrodrig2001@gmail.com>
2022-11-24 23:21:05 -0300

    soluciono el recargo de las cuotas a pagar

--- admin/src/core/payment/__init__.py
+++ admin/src/core/payment/__init__.py
@@ -36,11 +36,11 @@ def payments_impagos(id):
     return Payment.query.filter_by(associated_id=id).filter_by(state="I").all()
 
 
-def costo_total(costo_disciplines):
+def costo_total(costo_disciplines,mes):
     now = datetime.now()
     config = Config.query.first()
     total = config.month_value + costo_disciplines
-    if now.day >= 1 and now.day <= 10:
+    if ((now.day >= 1 and now.day <= 10 and mes == now.month) or (mes > now.month)):
         return total
     else:
         return total + (int(total * (config.recharge_percentaje / 100)))

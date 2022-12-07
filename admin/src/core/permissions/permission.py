21382b9c5be31cb72eb0d3029e158f7b4f4af555
Santiago dos Santos <santiiagodossantos@icloud.com>
2022-12-07 12:53:13 -0300

    Agrego doc strings

--- admin/src/core/permissions/permission.py
+++ admin/src/core/permissions/permission.py
@@ -2,10 +2,9 @@ from src.core.database import db
 
 
 class Permission(db.Model):
+    """A class to represent permission modeling"""
+
     __tablename__ = 'permissions'
     
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(255), unique=True, nullable=False)
-
-
-# Este archivo no deberia modificarse mas

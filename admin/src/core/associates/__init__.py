1dc31ea0c71a9e6eaefc0e1d78494a0397866a56
Martin Ezequiel Rodriguez <martinrodrig2001@gmail.com>
2022-12-07 11:48:54 -0300

    soluciono estadistica de cantidad de asociados

--- admin/src/core/associates/__init__.py
+++ admin/src/core/associates/__init__.py
@@ -242,12 +242,12 @@ def cantidadInscripcionesPorMes():
         #obtengo el numero de mes de la fecha de inscripcion
         mes = asociado.create_at.month
         #agrego el numero de mes al array
-        meses[mes]=meses[mes]+1
+        meses[mes-1]=meses[mes-1]+1
     #defino array con el nomrbe de cada mes
     nombresMeses = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"]
     #recorro nombre de meses y asigno a cada uno la cantidad de inscripciones
     total = []
-    for i in range(1,12):
+    for i in range(0,12):
         dic = {}
         dic["mes"] = nombresMeses[i]
         dic["cantidad"] = meses[i]

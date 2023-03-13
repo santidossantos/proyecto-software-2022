832362f52195ba1beb47f46467bec42b2386e67b
Santiago dos Santos <santiiagodossantos@icloud.com>
2023-03-13 20:06:57 -0300

    Development

--- portal/src/views/HomeView.vue
+++ portal/src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <PromptPwa/>
     <Home />
   </div>
 </template>
@@ -7,20 +8,24 @@
 <script>
 // @ is an alias to /src
 import Home from "@/components/Home.vue";
+import PromptPwa from "@/components/PromptPwa.vue";
 
 export default {
   name: "HomeView",
   components: {
     Home,
-  },
+    PromptPwa
+},
 };
 </script>
 
 <style>
+
+
+
 .home {
   width: 100%;
   height: 100vh;
-  background-color: #e2dfdf;
   font-family: "Share Tech", sans-serif;
   position: relative;
 }

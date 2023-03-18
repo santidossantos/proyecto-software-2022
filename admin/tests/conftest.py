2201fb4ea127bc1deae1559562a729a5f0c0be71
Santiago dos Santos <santiiagodossantos@icloud.com>
2023-03-17 22:33:55 -0300

    Conf Test

--- admin/tests/conftest.py
+++ admin/tests/conftest.py
@@ -10,12 +10,8 @@ def app():
         "TESTING": True
     })
 
-    # other setup can go here
-
     yield app
-
-    # clean up / reset resources here
-
+    
 
 @pytest.fixture()
 def client(app):
--- admin/tests/test_users.py
+++ admin/tests/test_users.py
@@ -1,15 +1,8 @@
 from src.core import auth
-from src.core.auth import User
-from src.core.database import db
 
 
 def test_new_user(app, fixture_auth):
-    """
-        GIVEN a User model
-        WHEN a new User is created
-        THEN check the email, hashed_password, and role fields are defined correctly
-    """
-
     assert fixture_auth.name == 'Matias'
-   
+
+
     

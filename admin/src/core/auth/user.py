b8f813bcb7be3ac7982b83944faf4c921f96d3ff
Santiago dos Santos <santiiagodossantos@icloud.com>
2023-03-19 19:22:21 -0300

    Development

--- admin/src/core/auth/user.py
+++ admin/src/core/auth/user.py
@@ -16,8 +16,8 @@ class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True, unique=True)
-    name = db.Column(db.String(50), unique=True)
-    last_name = db.Column(db.String(50), unique=True)
+    name = db.Column(db.String(50), unique=False)
+    last_name = db.Column(db.String(50), unique=False)
     email = db.Column(db.String(50), unique=True)
     user_name = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(256))
